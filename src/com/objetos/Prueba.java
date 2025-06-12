0e56b91c8f92ed6b11dbe3309ef6c01860c6dab6
elianartacho <elianartacho@gmail.com>
2025-06-12 13:22:00 +0200

    first commit

---
+++ src/com/objetos/Prueba.java
@@ -0,0 +1,9 @@
+package com.objetos;
+
+public class Prueba {
+    //esta clase no se va a ejecutar
+    //para probarlo vamos a crear un metodo para saludar
+    public static void saludar(){
+        System.out.println("Saludando desde prueba");
+    }
+}
